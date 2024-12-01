67bc342fba6aa8a0d57b4313a1d197870868cbf4
Mikhail Tikhonov <mikheilitikhonovi@gmail.com>
2024-12-01 20:57:03 +0400

    test

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>1232src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
