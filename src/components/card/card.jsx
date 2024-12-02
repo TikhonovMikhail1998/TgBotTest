5fe45874067891bb26accb0b2fc8f8128f0388fb
Mikhail Tikhonov <mikheilitikhonovi@gmail.com>
2024-12-02 15:37:54 +0400

    test

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -16,9 +16,9 @@ function Card() {
       </div>
      <div style={{width:'100%'}}></div>
      <div className='block1'>
-      <div className='info'>Имя: {window.Telegram.WebApp.initDataUnsafe?.user?.first_name}</div>
-      <div className='info'>Фамилия: {window.Telegram.WebApp.initDataUnsafe?.user?.last_name}</div>
-      <div className='info'>Username: {window.Telegram.WebApp.initDataUnsafe?.user?.username}</div>
+      <div >Имя: {window.Telegram.WebApp.initDataUnsafe?.user?.first_name}</div>
+      <div >Фамилия: {window.Telegram.WebApp.initDataUnsafe?.user?.last_name}</div>
+      <div >Username: {window.Telegram.WebApp.initDataUnsafe?.user?.username}</div>
       </div>
       </div>
      
